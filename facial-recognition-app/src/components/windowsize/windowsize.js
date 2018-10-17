24dc5dd9a6d4a5aae7a7b87ffa6ec1138a43f22f
Mike Thiem <Mike.Thiem@ihsmarkit.com>
2018-10-17 16:40:01 -0600

    trying to fix the layout uglyness

---
+++ facial-recognition-app/src/components/windowsize/windowsize.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import windowSize from 'react-window-size';
+ 
+class ScreenSize extends Component {
+ 
+  render() {
+    return (
+      <p>
+        Screen width is: {this.props.windowWidth}
+        <br />
+        Screen height is: {this.props.windowHeight}
+      </p>
+    );
+  }
+ 
+}
+ 
+export default windowSize(ScreenSize);
